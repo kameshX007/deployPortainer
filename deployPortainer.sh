bf9747d868df5c68f4c387dc9b26d45b71ed724b
kameshX007 <112748572+kameshX007@users.noreply.github.com>
2022-09-04 13:26:20 +0530

    Update deployPortainer.sh

--- deployPortainer.sh
+++ deployPortainer.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+#Checking & Removing existing container
 echo "Checking if Portainer already deployed...">deployPortainer.log;
 container=$(docker ps -q --filter ancestor=portainer/portainer-ce )>>deployPortainer.log;
 [ -z "$container" ] && echo "No running container found">>deployPortainer.log || docker rm -f $container;
+
+#Checking whether username is passed to script
+#A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "Please pass username...">>deployPortainer.log && exit 0 || echo "Deploying for user $dockerUser">>deployPortainer.log;
+[ -z "$dockerUser" ] && echo "###Please pass username...!!!">>deployPortainer.log && exit 0 || echo "Deploying for user $dockerUser">>deployPortainer.log;
 echo "Deploying Portainer for user $dockerUser...">>deployPortainer.log;
+
+#Deploying container
 sudo docker run -d -p 9000:9000 --name=portainer  --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /home/$dockerUser/docker/portainer/data:/data portainer/portainer-ce;
-echo "Portainer deployment successfull...">>deployPortainer.log;
+echo "###Portainer deployment successfull...!!!">>deployPortainer.log;
 exit 0
