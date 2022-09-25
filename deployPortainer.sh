d0b4347b84de24c5630ddb8ef3b160fda6530301
kameshX007 <kameshbook@gmail.com>
2022-09-25 13:35:10 +0530

    #

--- deployPortainer.sh
+++ deployPortainer.sh
@@ -2,14 +2,15 @@
 #Checking whether username is passed to script
 #A docker folder will be created inside this users home directory and container persistant data will be stored there
 dockerUser=$1;
-[ -z "$dockerUser" ] && echo "###Please pass the username...!!!" && exit 0 || echo "Deploying Portainer for user $dockerUser";
+[ -z "$dockerUser" ] && echo "Please pass the username...!!!" && exit 0 || echo "Deploying Portainer for user $dockerUser";
 
 #Checking & Removing existing container
 echo "Checking if Portainer already deployed...";
 container=$(docker ps -q --filter ancestor=portainer/portainer-ce );
+echo $container;
 [ -z "$container" ] && echo "No running container found" || docker rm -f $container;
 
 #Deploying container
 docker run -d -p 9000:9000 --network tunnel --name=portainer  --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /home/$dockerUser/docker/portainer/data:/data portainer/portainer-ce;
-echo "###Portainer deployment successfull...!!!";
+echo "Portainer deployment successfull...!!!";
 exit 0
